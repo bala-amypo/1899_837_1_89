14971efd1cc5538ef61000638ded481145ad873d
bala-amypo <bala@amypo.in>
2025-12-25 09:46:26 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 
-@SpringBootApplication // [cite: 784]
-@ServletComponentScan // Required to enable @WebServlet annotation for HelloServlet
+@SpringBootApplication
 public class DemoApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args); // [cite: 787]
+        SpringApplication.run(DemoApplication.class, args);
     }
+
 }
